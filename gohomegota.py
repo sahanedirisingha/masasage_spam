47ce4fbd0b060ae678a53125014e3b3bf6f52ab6
sahan edirisingha <75208139+sahanedirisingha@users.noreply.github.com>
2022-04-10 05:44:44 +0530

    Add files via upload
    if you want to  change spam you can change  list name as comments ,,,,

---
+++ gohomegota.py
@@ -0,0 +1,30 @@
+# go home gota
+
+import pyautogui
+import time
+
+# GoHomeGota #GoHomeGota2022 #gohomerajapaksas
+comments = ["#GoHomeGota", " #GoHomeGota2022", "#gohomerajapaksas"]
+
+time.sleep(5)
+
+while True:  #GoHomeGota
+
+
+
+    for i in range(3):
+        pyautogui.typewrite(comments[i])
+        pyautogui.typewrite("\n")
+        time.sleep(2)
+
+
+
+
+
+
+
+
+
+
+
+
